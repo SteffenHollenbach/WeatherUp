7de52f05382c65f3b9a0c0256cc12a433f1e3241
SteffenHollenbach <steffenhollenbach@web.de>
2015-07-26 11:51:06 +0200

    Anzeige auf Layout, Holen mit Button, wählen Zip-Code (nur DE)

--- app/src/main/java/com/example/steffen/weatherup/MainActivity.java
+++ app/src/main/java/com/example/steffen/weatherup/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -15,45 +19,49 @@ import retrofit.client.Response;
 public class MainActivity extends ActionBarActivity {
 
     String ENDPOINT = "http://api.openweathermap.org/data/2.5";
+    TextView tv_city_result, tv_temp_result, tv_weather_desc_result, tv_wind_speed_result, tv_humidity_result;
+    Button btn_get;
+    EditText et_zip;
+    WeatherService weatherservice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_city_result = (TextView) findViewById(R.id.tv_city_result);
+        tv_temp_result = (TextView) findViewById(R.id.tv_temp_result);
+        tv_weather_desc_result = (TextView) findViewById(R.id.tv_weather_desc_result);
+        tv_wind_speed_result = (TextView) findViewById(R.id.tv_wind_speed_result);
+        tv_humidity_result = (TextView) findViewById(R.id.tv_humidity_result);
+
+        btn_get = (Button) findViewById(R.id.btn_get);
+        et_zip = (EditText) findViewById(R.id.et_zip);
+
+
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(ENDPOINT)
                 .build();
 
-        WeatherService weatherservice = restAdapter.create(WeatherService.class);
+        weatherservice = restAdapter.create(WeatherService.class);
 
-        weatherservice.getWeatherZip("55435,de", "metric", new Callback<WeatherObject>() {
-
-            @Override
-            public void success(WeatherObject wo, Response response) {
-                Log.i("Gefunden", "True");
-                Log.i("Ergebnis", wo.toString());
-            }
+        btn_get.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void failure(RetrofitError error) {
-                Log.e("Gefunden", "Nein: " + error);
+            public void onClick(View arg0) {
+
+                String zip = et_zip.getText().toString();
+                getWeatherZip(zip + ",de", "metric");
+
             }
+
         });
 
-        /*weatherservice.getWeathersZip("55435,de", new Callback<List<WeatherObject>>() {
 
-            @Override
-            public void success(List<WeatherObject> weatherObjects, Response response) {
-                Log.i("Gefunden", "True");
-                Log.i("Ergebnis", weatherObjects.get(0).toString());
-            }
 
-            @Override
-            public void failure(RetrofitError error) {
-                Log.e("Gefunden", "Nein: " + error);
-            }
-        });*/
+
+
+
 
 
 
@@ -61,7 +69,29 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    public void getWeatherZip(String zip, String units){
+        weatherservice.getWeatherZip(zip, units, new Callback<WeatherObject>() {
 
+            @Override
+            public void success(WeatherObject wo, Response response) {
+                Log.i("Gefunden", "True");
+                if(wo.cod.equals("200")) {
+                    tv_city_result.setText(wo.name);
+                    tv_temp_result.setText(wo.main.temp + " °C");
+                    tv_weather_desc_result.setText(wo.weather.get(0).description);
+                    tv_wind_speed_result.setText(wo.wind.speed + " km/h");
+                    tv_humidity_result.setText(wo.main.humidity + " %");
+                } else {
+                    tv_city_result.setText("Fehler");
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("Gefunden", "Nein: " + error);
+            }
+        });
+    }
 
 
 
