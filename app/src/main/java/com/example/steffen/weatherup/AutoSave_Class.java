f5fc18f6e47a2fe7cb9c05f5222ecdb147e8c304
SteffenHollenbach <steffenhollenbach@web.de>
2015-07-28 01:00:49 +0200

    Start / Stop Service Anzeige

--- app/src/main/java/com/example/steffen/weatherup/AutoSave_Class.java
+++ app/src/main/java/com/example/steffen/weatherup/AutoSave_Class.java
@@ -1,8 +1,10 @@
 package com.example.steffen.weatherup;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,7 @@ public class AutoSave_Class extends Activity{
     Button btn_start, btn_stop;
     CheckBox cb_running;
     Context c;
+    static SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,12 @@ public class AutoSave_Class extends Activity{
         btn_stop = (Button) findViewById(R.id.btn_stop);
         cb_running = (CheckBox) findViewById(R.id.cb_running);
         c = this;
+
+        prefs = c.getSharedPreferences(
+                "Share", Context.MODE_PRIVATE);
+
+        refreshCeckBox();
+
 
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +66,7 @@ public class AutoSave_Class extends Activity{
     }
 
     public void refreshCeckBox(){
-        cb_running.setChecked(SaveReciever.prefs.getBoolean("AlarmSet", false));
+        cb_running.setChecked(prefs.getBoolean("AlarmSet", false));
     }
 
     public void startIntent(String s){
@@ -65,6 +74,16 @@ public class AutoSave_Class extends Activity{
         Intent intent = new Intent();
         intent.setAction(s);
         sat.onReceive(c, intent);
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
