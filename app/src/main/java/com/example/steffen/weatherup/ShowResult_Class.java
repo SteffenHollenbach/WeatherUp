8d8d3fa0b1e36274a17ca532fc65d47330d2d88c
SteffenHollenbach <steffenhollenbach@web.de>
2015-07-30 18:34:40 +0200

    share Button Result

--- app/src/main/java/com/example/steffen/weatherup/ShowResult_Class.java
+++ app/src/main/java/com/example/steffen/weatherup/ShowResult_Class.java
@@ -1,9 +1,12 @@
 package com.example.steffen.weatherup;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableRow;
@@ -109,5 +112,35 @@ public class ShowResult_Class extends ActionBarActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.share) {
+
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "The weather in " +  wo.getName() + " is " + wo.getWeather().get(0).getDescription() + ".";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
